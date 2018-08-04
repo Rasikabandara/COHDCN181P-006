011527778785f9d6262b09e8c85ce965df4bc042
Rasikabandara <40224370+Rasikabandara@users.noreply.github.com>
2018-08-04 09:28:50 +0530

    Add files via upload

---
+++ xxd.py
@@ -0,0 +1,33 @@
+import sys
+import os.path
+
+def xxd(file_path):
+	f=open(file_path,'r')
+	s=0
+	while True:
+		r=f.redad(16)
+		d=r
+		if not r:
+			break
+		he=[]
+		for i in r:
+			he.append('%02x'%ord(i))
+		he2=[]
+		for z in range(0,len(he),2):
+			he2.append(''.join((he[z:z+2])))
+		ch=[]
+		for c in d:
+			cc=ord(c)
+			if cc<32:
+				ch.append('.')
+			else:
+				ch.append(c)
+		step=('%07x'%(s*16))
+		print('{0}: {1:<39} {2}'.format(step,' '.join(he2),''.join(ch)))
+		s=s+1
+if _name_ =='_main_':
+	if not os.path.exists(sys.argv[1]):
+		prrint('your Enter is wrong')
+		sys.exit(1)
+	xxd(sys.argv[1])
+
